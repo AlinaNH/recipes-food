cdfb3d843ebb9b9fb2a89bc7370dd39595ed7475
AleksandrGogolushko <gogolushko@outlook.com>
2020-11-13 10:03:03 +0300

    add recipes card in results page, auto load new card, and transition after render

--- front/Header/Authentication/Authentication.tsx
+++ front/Header/Authentication/Authentication.tsx
@@ -8,16 +8,18 @@ interface AuthenticationProps extends RouteComponentProps {
   operation: string;
 }
 
+
 export const Authentication = ({
   history,
   operation,
+  location
 }: AuthenticationProps): any => {
   return (
     <div className="authentication-container-fade">
       <div className="authentication-container">
         <span
           className="authentication-container-close"
-          onClick={() => history.push('/')}
+          onClick={() => history.push(`/`)}
         >
           &times;
         </span>
--- front/ResultsPage/ResultsPage.tsx
+++ front/ResultsPage/ResultsPage.tsx
@@ -2,10 +2,26 @@ import * as React from 'react';
 import { Filter } from './Filter/Filter';
 import { Results } from './Results/Results';
 import './ResultsPage.css';
+import { motion } from 'framer-motion';
+
+const PageVariants = {
+  in: {
+    opacity: 1,
+  },
+  out: {
+    opacity: 0,
+  },
+};
 
 export const ResultsPage: React.FunctionComponent = () => (
-  <div className="ResultsPage">
+  <motion.div
+    exit="out"
+    animate="in"
+    initial="out"
+    variants={PageVariants}
+    className="ResultsPage"
+  >
     <Filter />
     <Results />
-  </div>
+  </motion.div>
 );
